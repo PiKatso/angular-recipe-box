775a951d2c73a6e4fb126700f3c427241e95bd9a
Kat Gifford and Sean Pierce <sean@>
2017-06-06 08:56:37 -0700

    add basic front end to recipe box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+// Root Component //
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div>
+    <h1>Recipe Box</h1>
+    <ul>
+      <p *ngFor="let thisRecipe of recipies">{{thisRecipe.title}} ~ {{thisRecipe.ingredients}}</p>
+    </ul>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
+
+// Class Declaration //
 export class AppComponent {
-  title = 'app works!';
+
+  recipies: Recipe[] = [
+    new Recipe ('Avacado Toast', ["bread", "avacado", "salt", "lemon", "pepper"]),
+    new Recipe ('Healthy Cookies', ["oatmeal", "bananas", "chocolate chips"])
+  ];
+}
+
+
+// Model Declaration //
+export class Recipe {
+  constructor(public title: string, public ingredients: string[]) {}
 }
